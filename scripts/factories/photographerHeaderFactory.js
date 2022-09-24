7d377ae5ac3366bc2a0c21d157d31c23effa8bf3
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2022-09-24 20:09:49 +0200

    photographerHeaderFactory, formatting

--- scripts/factories/photographerHeaderFactory.js
+++ scripts/factories/photographerHeaderFactory.js
@@ -7,9 +7,11 @@ export function photographerHeaderFactory(data) {
 
         // TODO change css class
         return document.createRange().createContextualFragment(
-                            `<h1>${name}</h1>
-                            <p class="photographer_section__location">${city}, ${country}</p>
-                            <p class="photographer_section__tagline">${tagline}s</p>
+                            `<div>   
+                                <h1>${name}</h1>
+                                <p class="photograph-header__location">${city}, ${country}</p>
+                                <p class="photograph-header__tagline">${tagline}s</p>
+                            </div>
                             <img src="assets/photographers/${portrait}" alt="portrait de ${portrait}">`);
     };
 
