a2b8c04c8cb6d332a958dfe038da35de0f5cffdb
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2022-09-28 23:10:11 +0200

    Contact modal form, js, missing validation

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,7 +3,7 @@ const displayModal = () => {
   modal.style.display = "block";
 };
 
-const closeModal= () => {
+const closeModal = () => {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 };
@@ -14,26 +14,26 @@ const modalFactory = (name) => {
   
       return document.createRange().createContextualFragment(
             `<div id="contact_modal">
-              <div role="dialog" aria-labelledby="dialogHeader" aria-describedby="dialogHeader" class="modal">
-                <header>
+              <div class="modal" role="dialog" aria-labelledby="dialogHeader" aria-describedby="dialogHeader">
+                <header id="modal__header">
                   <h2 id="dialogHeader">Contactez-moi ${name}</h2>
                   <img src="assets/icons/close.svg" onclick="closeModal()"/>
                 </header>
-                <form name="" action="photgrapher.html" method="post" onsubmit="return false">
+                <form id="modal__form" name="contact-form" action="" method="post" onsubmit="validate(event); return false;">
                   <div>
                     <label for="first-name">Prénom</label>
-                    <input aria-labelledby="First Name" name="first-name" id="first-name"/>
+                    <input id="first-name" aria-labelledby="First Name" name="first-name""/>
 
                     <label for="last-name">Nom</label>
-                    <input aria-labelledby="Last Name" name="last-name" id="first-name"/>
+                    <input id="last-name" aria-labelledby="Last Name" name="last-name"/>
 
                     <label for="email">Email</label>
-                    <input aria-labelledby="Email" name="email" id="email" required/>
+                    <input id="email" aria-labelledby="Email" name="email" required/>
 
                     <label for="message">Votre message</label>
-                    <textarea aria-labelledby="Your Message" name="message" id="message" rows="8"></textarea>
+                    <textarea id="message" aria-labelledby="Your Message" name="message" rows="8"></textarea>
                   </div>
-                  <button class="contact_button">Envoyer</button>
+                  <button class="contact_button" type="submit">Envoyer</button>
                 </form>
               </div>
             </div>`);
@@ -41,3 +41,61 @@ const modalFactory = (name) => {
   
     return { getModalDOM }
   };
+
+const form = [
+  {
+    element: document.getElementById('first-name'),
+    errorMessage: 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.',
+    validation: function() {
+      return /^[a-z][a-z]+$/i.test(this.element.value.trim());
+    }
+  },
+  {
+    element: document.getElementById('last-name'),
+    errorMessage: 'Veuillez entrer 2 caractères ou plus pour le champ du nom.',
+    validation: function() {
+      return /^[a-z][a-z]+$/i.test(this.element.value.trim());
+    }
+  },
+  {
+      element: document.getElementById('email'),
+      errorMessage: 'Veuillez entrer une adresse mail valide.',
+      validation: function() {
+          return /^[\w]+@[\w]+\.[\w]+$/.test(this.element.value);
+      }
+  }
+];
+
+let valid = true;
+const validate = (event) => {
+  // form.forEach(element => {
+  //   // if(!element.validation()) { 
+  //   //   console.log(element.errorMessage);
+  //   //   valid = false; 
+  //   // }
+  //   console.log(element);
+  // });
+
+  if(valid) {
+    showValidationMessage();
+    showCloseButton();
+  }
+};
+
+const showValidationMessage = () => {
+  const validationMessage = document.createElement('div');
+  validationMessage.classList.add('modal__validation');
+  validationMessage.textContent = "Message envoyé !";
+  document.querySelector('#modal__form').style.visibility = 'hidden';
+  document.querySelector('#modal__header').style.visibility = 'hidden';
+  document.querySelector('.modal').appendChild(validationMessage);
+};
+
+const showCloseButton = () => {
+    const closeValidationButton = document.createElement('button');
+    closeValidationButton.textContent = "Fermer";
+    closeValidationButton.classList.add('contact_button');
+    closeValidationButton.addEventListener("click", closeModal);
+    console.log(closeValidationButton);
+    document.querySelector('.modal').appendChild(closeValidationButton);
+};
