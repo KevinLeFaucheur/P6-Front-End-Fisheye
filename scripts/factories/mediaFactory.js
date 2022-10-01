4b7385825d8460a06d6072021a694fcc645f1b54
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2022-10-01 21:11:30 +0200

    Likes system updating, Video handling

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -3,14 +3,28 @@ export const mediaFactory = (data, name) => {
 
   const getMediaDOM = () => {
     const folderName = name.match(/[a-z]+[-][a-z]+|[a-z]+/i);
+    
+    const imageFragment = 
+        `<article>
+            <img class="media-section__image" src="assets/medias/${folderName}/${image}" alt="${title}, closeup view" onclick="displayLightbox()"></img>
+            <div class="media-section__body">
+                <span>${title}</span><span class="likes" id="${id}">${likes} <i class="media-section__like fa-solid fa-heart"></i></span>
+            </div>
+          </article>`;
 
-    return document.createRange().createContextualFragment(
-          `<article>
-              <img class="media-section__image" src="assets/medias/${folderName}/${image}" alt="${title}, closeup view" onclick="displayLightbox()"></img>
-              <div class="media-section__body">
-                  <span>${title}</span><span class="likes" id="${id}">${likes} <i class="media-section__like fa-solid fa-heart"></i></span>
-              </div>
-          </article>`);
+    const videoFragment = 
+        `<article>
+          <video class="media-section__video">
+            <source src="assets/medias/${folderName}/${video}" type="video/mp4">
+          </video>
+          <div class="media-section__body">
+              <span>${title}</span><span class="likes" id="${id}">${likes} <i class="media-section__like fa-solid fa-heart"></i></span>
+          </div>
+        </article>`;
+
+    const mediaFragment = data.hasOwnProperty('image') ? imageFragment : videoFragment; 
+
+    return document.createRange().createContextualFragment(mediaFragment);
   };
 
   return { id, photographerId, title, image, video, likes, date, price, getMediaDOM }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,21 +3,17 @@ import { getPhotographers } from "../utils/getPhotographerData.js";
 import { photographerHeaderFactory } from "../factories/photographerHeaderFactory.js";
 import { pricingInsertFactory } from "../factories/pricingInsertFactory.js";
 import { mediaFactory } from "../factories/mediaFactory.js";
+import { sortingFactory } from "../factories/sortingFactory.js";
 
 const currentPageParamId = parseInt(new URLSearchParams(window.location.search).get('id'));
 const photographersData = await getPhotographers();
 const currentPagePhotographerData = photographersData.photographers.filter(object => object.id == currentPageParamId)[0];
 
-const getMediasByPhotographerId = async (data, id) => {
+const getMediasByPhotographerId = (data, id) => {
     return [...data.media].filter(element => element.photographerId == id);
 };
-// const mediasData = await getMediasByPhotographerId(photographersData, currentPageParamId);
 
-const addLike = (imageId, likeCount) => {
-    document.getElementById(imageId).innerHTML = likeCount++ + ' <i class="media-section__like fa-solid fa-heart"></i>';
-};
-
-const displayHeaderData = async () => {
+const displayHeaderData = () => {
     const photographerHeader = document.querySelector(".photograph-header");
     const photographerHeaderData = photographerHeaderFactory(currentPagePhotographerData);
     const photographerHeaderDOM = photographerHeaderData.getHeaderDOM();
@@ -25,7 +21,7 @@ const displayHeaderData = async () => {
     photographerHeader.appendChild(photographerHeaderDOM);
 };
 
-const displayMediaData = async () => {
+const displayMediaData = () => {
     const main = document.getElementById('main');
     const mediaSection = document.createElement('section');
     mediaSection.classList.add('media-section');
@@ -33,7 +29,7 @@ const displayMediaData = async () => {
     main.appendChild(mediaSection);
 
     // get current page medias by photographer id
-    const mediasData = await getMediasByPhotographerId(photographersData, currentPageParamId);
+    const mediasData = getMediasByPhotographerId(photographersData, currentPageParamId);
     
     mediasData.forEach(media => {
         const mediaDOM = mediaFactory(media, currentPagePhotographerData.name);
@@ -41,28 +37,46 @@ const displayMediaData = async () => {
         mediaSection.appendChild(mediaDOM.getMediaDOM());
     });
 
-    const medias = document.querySelectorAll('.media-section__image');
+    const medias = document.querySelectorAll('.media-section__image, .media-section__video');
     medias.forEach(image => image.addEventListener('click', displayLightbox));
 
     const likes = document.querySelectorAll('.media-section__like');
-    likes.forEach(element => element.addEventListener('click', addLike(element.parentElement.id, parseInt(element.parentElement.textContent))));
+    likes.forEach(element => element.addEventListener('click', () => addLike(element.parentElement.id, parseInt(element.parentElement.textContent))));
 };
 
 const getTotalLikes = () => {
     let totalLikes = 0;
-    const likes = document.querySelectorAll('.likes');
-    likes.forEach(element => totalLikes += parseInt(element.textContent));
-    console.log(likes);
+    document.querySelectorAll('.likes').forEach(element => totalLikes += parseInt(element.textContent));
     
     return totalLikes;
-}
+};
 
-const displayInsertData = async () => {
+const updateTotalLikes = () => {
+    document.getElementById('pricing-insert__likes').innerHTML = `${getTotalLikes()} <i class="fa-solid fa-heart"></i>`;
+};
+
+const addLike = (imageId, likeCount) => {
+    document.getElementById(imageId).innerHTML = ++likeCount + ' <i class="media-section__like fa-solid fa-heart"></i>'; // ++
+    updateTotalLikes();
+};
+
+const displayInsertData = () => {
     const main = document.getElementById('main');
     const pricingInsertData = pricingInsertFactory(currentPagePhotographerData, getTotalLikes());
     const pricingInsertDOM = pricingInsertData.getInsertDOM();
 
     main.appendChild(pricingInsertDOM);
+};
+
+const displaySortingData = () => {
+    const main = document.getElementById('main');
+
+    const mediasData = getMediasByPhotographerId(photographersData, currentPageParamId); // DYR
+
+    const sortingData = sortingFactory(mediasData);
+    const sortingDOM = sortingData.getSortingDOM();
+
+    main.appendChild(sortingDOM);
 };
 
 const initModal = () => {
@@ -77,6 +91,7 @@ const initLightbox = () => {
 
 const init = async () => {
     displayHeaderData();
+    displaySortingData();
     displayMediaData();
     displayInsertData();
     initModal();
